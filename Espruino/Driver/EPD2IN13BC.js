352d002fd8ff442f270a7286b587602f33779361
Larry da Ponte <larry.daponte@gmail.com>
2022-11-06 12:31:52 -0800

    Test

--- Espruino/Driver/EPD2IN13BC.js
+++ Espruino/Driver/EPD2IN13BC.js
@@ -1,5 +1,5 @@
 function EPD2IN13BC (config, spi) {
-  this.driverVersion = "v1.07";
+  this.driverVersion = "v1.08";
   this.resetPin = config.resetPin;
   this.dcPin = config.dcPin;
   this.csPin = config.csPin;
